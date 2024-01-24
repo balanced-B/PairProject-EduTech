b711946dd8a3ec3cf7b842060094d8030411c4a0
balanced-B <ariobimo.mp@gmail.com>
2024-01-25 02:34:01 +0700

    starter

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PairProject-EduTech
+
+semua package dasar udah di install, termasuk bcryptjs.
+dan jangan lupa kalo username dan password beda untuk postgres, ganti dulu di config -b
---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bcrypt = require('bcryptjs')
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:true}))
+
+app.get('/', (req, res) => { // langsung redirect ke home/landing page ***bahas belakangan
+  res.send('Hello World!')
+})
+
+app.get('/register', (req, res) => { // page untuk daftar user (form)
+  res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => { // redirect untuk log in
+  res.send('Hello World!')
+})
+
+app.get('/login', (req, res) => { // page untuk log in
+  res.send('Hello World!')
+})
+
+app.post('/login', (req, res) => { // redirect landing page/home ***bahas belakangan
+  res.send('Hello World!')
+})
+
+app.get('/userProfile', (req, res) => { // display profile user
+  res.send('Hello World!')
+})
+
+app.get('/courses', (req, res) => { // display courses
+  res.send('Hello World!')
+})
+
+app.get('/categories', (req, res) => { // display categories ***jika mau dan diperlukan
+  res.send('Hello World!')
+})
+
+// sementara begini, lanjut diskusi nanti pagi
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
